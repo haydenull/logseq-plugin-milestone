9ef7358e32c7cdcbad890be352a288a9bc1a3e8c
haydenull <brooklyn1984@qq.com>
2022-01-10 19:37:55 +0800

    wip: 🚧 wip

--- src/util.ts
+++ src/util.ts
@@ -1,13 +1,18 @@
 import { BlockEntity } from '@logseq/libs/dist/LSPlugin.user'
-import moment from 'moment'
+import moment, { Moment } from 'moment'
+
+const getDateFromText = (text: string): Moment => {
+  // return /\[\[(.+?)\]\]/g.exec(text)
+}
 
 export const getMilestones = (content: BlockEntity) => {
   const milestones = content?.children
           ?.find(item => (item as BlockEntity )?.content === 'milestones')
   return (milestones as BlockEntity)?.children?.map(milestone => {
+    const content = (milestone as BlockEntity)?.content
     return {
-      content: (milestone as BlockEntity)?.content,
-      // date: (milestone as BlockEntity)?.date,
+      content,
+      date: getDateFromText(content),
     }
   })
 }
